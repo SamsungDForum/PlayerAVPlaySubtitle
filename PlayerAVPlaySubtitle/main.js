d99b20b950d6dfb80f011d7406cadc56bfb350dd
Hyojin Kim <hyojins.kim@samsung.com>
2018-02-27 20:37:09 +0900

    Add exception code for every avplay api

--- PlayerAVPlaySubtitle/main.js
+++ PlayerAVPlaySubtitle/main.js
@@ -5,7 +5,7 @@ function handleKeydown(event) {
 		case 10009:
 			console.log('[TestApp] return');
 			tizen.application.getCurrentApplication().exit();
-			
+
 		break;
         default:
 
@@ -26,33 +26,38 @@ var listener = {
 }
 
 function test0() {
-	webapis.avplay.close();
-	webapis.avplay.open(videoURL);
-	webapis.avplay.setListener(listener);
+	try {
+		webapis.avplay.close();
+		webapis.avplay.open(videoURL);
+		webapis.avplay.setListener(listener);
 
-	if(subtitleURL == ''){
-		webapis.avplay.setSilentSubtitle(true);
-	} else {
-		var subtitleFileName = 'subtitle' + new Date().getTime();
-		var download = new tizen.DownloadRequest(subtitleURL, 'wgt-private-tmp', subtitleFileName);
+		if(subtitleURL == ''){
+			webapis.avplay.setSilentSubtitle(true);
+		} else {
+			var subtitleFileName = 'subtitle' + new Date().getTime();
+			var download = new tizen.DownloadRequest(subtitleURL, 'wgt-private-tmp', subtitleFileName);
 
-		tizen.download.start(download, {
-			oncompleted: function(downloadId, fullPath){
-				tizen.filesystem.resolve('wgt-private-tmp', 
-					function(e){
-						var packageURI = e.toURI().substring(7);
-						webapis.avplay.setExternalSubtitlePath(packageURI + '/' + subtitleFileName + '.smi');
-					},
-					function(e){
-						console.log(e);
-					}, 'r');
-			}
-		});
+			tizen.download.start(download, {
+				oncompleted: function(downloadId, fullPath){
+					tizen.filesystem.resolve('wgt-private-tmp',
+						function(e){
+							var packageURI = e.toURI().substring(7);
+							webapis.avplay.setExternalSubtitlePath(packageURI + '/' + subtitleFileName + '.smi');
+						},
+						function(e){
+							console.log(e);
+						}, 'r');
+				}
+			});
 
-		webapis.avplay.prepare();
-		var AVPlayer = document.getElementById('av-player');	
-		webapis.avplay.setDisplayRect(AVPlayer.offsetLeft, AVPlayer.offsetTop, AVPlayer.offsetWidth, AVPlayer.offsetHeight);
-		webapis.avplay.play();
+			webapis.avplay.prepare();
+			var AVPlayer = document.getElementById('av-player');
+			webapis.avplay.setDisplayRect(AVPlayer.offsetLeft, AVPlayer.offsetTop, AVPlayer.offsetWidth, AVPlayer.offsetHeight);
+			webapis.avplay.play();
+		}
+	}
+	catch(e) {
+		console.log('test0 throw exception : ' + JSON.stringify(e));
 	}
 }
 
@@ -65,57 +70,77 @@ function test2() {
 }
 
 function test3() {
-	var tracks = webapis.avplay.getTotalTrackInfo();
-	
-	for (var i = 0; i < tracks.length; i++) {
-		if (tracks[i].type == 'TEXT') {
-			text = 'index: ' + tracks[i].index + ', type: ' + tracks[i].type + ', extra_info: ' + tracks[i].extra_info;
-			log(text);
+	try {
+		var tracks = webapis.avplay.getTotalTrackInfo();
+
+		for (var i = 0; i < tracks.length; i++) {
+			if (tracks[i].type == 'TEXT') {
+				text = 'index: ' + tracks[i].index + ', type: ' + tracks[i].type + ', extra_info: ' + tracks[i].extra_info;
+				log(text);
+			}
 		}
+	}
+	catch(e) {
+		console.log('test3 throw exception : ' + JSON.stringify(e));
 	}
 }
 
 function test4() {
-	var tracks = webapis.avplay.getCurrentStreamInfo();
+	try {
+		var tracks = webapis.avplay.getCurrentStreamInfo();
 
-	for (var i = 0; i < tracks.length; i++) {
-		if (tracks[i].type == 'TEXT') {
-			text = 'index: ' + tracks[i].index + ', type: ' + tracks[i].type + ', extra_info: ' + tracks[i].extra_info; 
-			log(text);
-			break;
-		}
-	}
-}
-
-function test5() {
-	var totalTracks = webapis.avplay.getTotalTrackInfo();
-	var currentTracks = webapis.avplay.getCurrentStreamInfo();
-	var currentIndex;
-	
-	for (var i = 0; i < currentTracks.length; i++) {
-		if(currentTracks[i].type == 'TEXT'){
-			 currentIndex = currentTracks[i].index;
-			 
-			 break;
-		}
-	}
-	
-	for (var i = 0; i < totalTracks.length; i++) {
-		if(totalTracks[i].type == 'TEXT') {
-			if(totalTracks[i].index != currentIndex) {
-				webapis.avplay.setSelectTrack('TEXT', totalTracks[i].index);
-				
-				text = 'Change to index: ' + totalTracks[i].index;
+		for (var i = 0; i < tracks.length; i++) {
+			if (tracks[i].type == 'TEXT') {
+				text = 'index: ' + tracks[i].index + ', type: ' + tracks[i].type + ', extra_info: ' + tracks[i].extra_info;
 				log(text);
-				
 				break;
 			}
 		}
 	}
+	catch(e) {
+		console.log('test4 throw exception : ' + JSON.stringify(e));
+	}
+}
+
+function test5() {
+	try {
+		var totalTracks = webapis.avplay.getTotalTrackInfo();
+		var currentTracks = webapis.avplay.getCurrentStreamInfo();
+		var currentIndex;
+
+		for (var i = 0; i < currentTracks.length; i++) {
+			if(currentTracks[i].type == 'TEXT'){
+				 currentIndex = currentTracks[i].index;
+
+				 break;
+			}
+		}
+
+		for (var i = 0; i < totalTracks.length; i++) {
+			if(totalTracks[i].type == 'TEXT') {
+				if(totalTracks[i].index != currentIndex) {
+					webapis.avplay.setSelectTrack('TEXT', totalTracks[i].index);
+
+					text = 'Change to index: ' + totalTracks[i].index;
+					log(text);
+
+					break;
+				}
+			}
+		}
+	}
+	catch(e) {
+		console.log('test5 throw exception : ' + JSON.stringify(e));
+	}
 }
 
 function test6(sync) {
-	webapis.avplay.setSubtitlePosition(sync);
+	try {
+		webapis.avplay.setSubtitlePosition(sync);
+	}
+	catch(e) {
+		console.log('test6 throw exception : ' + JSON.stringify(e));
+	}
 }
 
 function main() {
